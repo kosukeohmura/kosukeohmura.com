39890243ffb62c3d230e94d34d5a02bdab5b924d
Kosuke Ohmura <kosuke.omura@every.tv>
2021-02-13 14:19:04 +0900

    Add ext-link component

---
+++ src/components/ext-link.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const ExtLink = ({ children, ...props }) => (
+  <a {...props} rel="noopener" target={props.target || '_blank'}>
+    {children}
+  </a>
+)
+
+export default ExtLink

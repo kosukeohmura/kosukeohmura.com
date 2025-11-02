6818e9b0f5cafe8d8a78f067c4f593a969953870
kosukeohmura <little-parrot@hotmail.co.jp>
2025-11-02 14:52:17 +0700

    Fix the app description

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -15,12 +15,12 @@ const Bio = () => {
     <div className="bio">
       <p>Software engineer from Japan, currently based in Thailand.</p>
       <p>
-        Developer of{" "}
+        Creator of{" "}
         <ExtLink href="https://flexscoreboard.kosukeohmura.com/">
           <FlexScoreboard className="bio-text-app-icon" />
           FlexScoreboard
         </ExtLink>
-        .
+        , a free scoreboard app.
       </p>
     </div>
   )
